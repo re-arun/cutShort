58f88edd623ca9a9c1afaf03e43bd7df226eb3eb
re-arun <re-arun@indicchain.com>
2022-07-29 02:32:57 +0530

    Styling

--- src/components/ShippingOption.js
+++ src/components/ShippingOption.js
@@ -16,49 +16,49 @@ const GetShippingOption = (props) => {
 
   return (
     <Container>
-    <Form >
-      <h3 > How are you planning to use Eden? </h3>
-      <h6>
-        We'll streamline your setup experience accordingly.
-      </h6>
-      <Container className="d-flex p-2 bd-highlight">
-        <Row>
-          <Col>
-            <Card style={{ width: "18rem" }}>
-              <Card.Img variant="top" src="holder.js/100px180" />
-              <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-                <Card.Text>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col>
-            <Card style={{ width: "18rem" }}>
-              <Card.Img variant="top" src="holder.js/100px180" />
-              <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-                <Card.Text>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
-              </Card.Body>
-            </Card>
-          </Col>
-        </Row>
-      </Container>
+      <Form>
+        <h3 className="text-center"> How are you planning to use Eden? </h3>
+        <h6 className="text-center text-secondary mb-5">
+          We'll streamline your setup experience accordingly.
+        </h6>
+        <Container className="justify-content-center">
+          <Row xs lg="2">
+            <Col>
+              <Card style={{ width: "18rem" }}>
+                <Card.Img variant="top" src="holder.js/100px180" />
+                <Card.Body>
+                  <Card.Title>Card Title</Card.Title>
+                  <Card.Text>
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </Card.Text>
+                  <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col>
+              <Card style={{ width: "18rem" }}>
+                <Card.Img variant="top" src="holder.js/100px180" />
+                <Card.Body>
+                  <Card.Title>Card Title</Card.Title>
+                  <Card.Text>
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </Card.Text>
+                  <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+        </Container>
 
-      <Button
-        onClick={() => val.nextPage()}
-        style={{ color: "white", width: "100%", background: "#6b40d8 " }}
-      >
-        Create Workspace
-      </Button>
-    </Form>
+        <Button
+          onClick={() => val.nextPage()}
+          style={{ color: "white", width: "100%", background: "#6b40d8 " }}
+        >
+          Create Workspace
+        </Button>
+      </Form>
     </Container>
   );
 };
